fd792ac32b365df5b32706e6c1105feb6528932b
Alvarez <alvxrz.rip@gmail.com>
2026-05-02 09:24:41 -0600

    feat: Pagina 1 VinculaTec

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import App from "./App";
 
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-
-// comentario de pruebillini
-
-
-//andando comentando a lo random nuevamente juas juas 
-
-
-
-
-
-
-
-//sigo modificando juas juas 
 registerRootComponent(App);
