a493ce93c88eee5b4b1fa5d45b34da6e91b9f8f1
Oscar <ritogt06@gmail.com>
2026-05-12 12:56:21 -0600

    En el archivo de server cambié el puerto que me aparecía que estaba en 3001 y al hacer el cambio en el .env no me agarraba por eso pero ya lo solucioné con este cambio

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const authRoutes  = require("./routes/auth");
 const citasRoutes = require("./routes/citas");
 
 const app  = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(cors());
 app.use(express.json());
